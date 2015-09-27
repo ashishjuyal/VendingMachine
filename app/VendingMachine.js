26381fd0875deabbe72a3f08e62cc44a4f4ee7dd
Ashish Juyal <ashish.juyal@gmail.com>
2015-09-27 20:53:30 +0200

    passed test for soda type coke

--- app/VendingMachine.js
+++ app/VendingMachine.js
@@ -1,11 +1,15 @@
 var vendingMachine = function() {
 	return {
 		messageOnScreen: "Hello!! Quench your thirst",
-		takeMoneyAndGiveSoda: function(money){
-			if(money !== undefined && money !== 0)
-				return 1;
+		takeMoneyAndGiveSoda: function(money, sodaType){
+			if(money !== undefined && money !== 0){
+				if(sodaType === 'Coke'){
+					return sodaCanCoke;
+				}
+			}
 		}
 	};
 }
+var sodaCanCoke = { sodaType: 'Coke', quantity: 1 };
 
 module.exports = vendingMachine;
